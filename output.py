b9cc98a458e0ef8c972e11fc9e3c4dffac7d61df
Ybs0127 <syb0127@naver.com>
2025-03-18 17:12:29 +0900

    first

---
+++ output.py
@@ -0,0 +1,25 @@
+#하나만 출력합니다.
+print("# 하나만 출력합니다.")
+print("Hello Python Programming...!")
+print()
+
+#여러개를 출력합니다.
+print("# 여러개를 출력합니다.")
+print("10, 20, 30, 40, 50")
+print("안녕하세요", "저의", "이름은", "윤인성입니다!")
+print()
+
+# 아무 것도 입력하지 않으면 단순하게 줄바꿈합니다.
+print("# 아무 것도 입력하지 않으면 단순하게 줄바꿈합니다.")
+print("---확인 전용선---")
+print()
+print()
+print("---확인 전용선---")
+
+input_a = float(input("첫 번째 숫자> "))
+input_b = float(input("두 번째 숫자> "))
+
+print("덧셈 결과:", input_a + input_b)
+print("뺄셈 결과:", input_a - input_b)
+print("곱셈 결과:", input_a * input_b)
+print("나눗셈 결과:", input_a / input_b)
